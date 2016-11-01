983aa06a9f685a0d7967911b224f322024e08d73
David Portabella <david.portabella@gmail.com>
2016-11-01 12:48:53 +0100

    Friends_Smokers example: added smoking-test.db

--- Examples/Weight_Learning/Friends_Smokers/run-mm.sh
+++ Examples/Weight_Learning/Friends_Smokers/run-mm.sh
@@ -8,3 +8,12 @@ lomrf wlearn \
 	-o smoking-learned.mln \
 	-ne Smokes/1,Cancer/1 \
 	-lossAugmented
+
+# Inference
+lomrf infer \
+	-inferType map \
+	-mapType ilp \
+	-i smoking-learned.mln \
+	-r map.result \
+	-e smoking-test.db \
+	-q Smokes/1,Cancer/1
